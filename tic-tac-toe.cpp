af7217aaa2290b1c0a125f17f18eebb0725b37a0
reticent-lily <bryantchen1807@gmail.com>
2025-04-22 01:19:41 -0400

    Update tic-tac-toe.cpp
    2 player ttt

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -3,10 +3,17 @@
 #include <array>
 #include <string>
 
+#include <cstdlib>
+
+
 //returns array index given a move
+// rework into take_space function --> will be used to check if the space is available, and take it
 int get_move();
 //self-explanatory
 void print_board(std::array<int,9> ar);
+
+bool validMove(std::array<int, 9>& ar, int move, bool turn);
+bool check_gaming(std::array<int, 9> ar, bool turn);
 
 /*
 ply1 - bool representing the turn
@@ -17,19 +24,40 @@ false = player 2 turn (o)
 */
 
 int main() {
-    bool gaming = true, ply1 = true;
-
+    bool gaming = true, ply1;
+    srand(0);
+    ply1 = rand() %2 == 0;
     std::array<int, 9> game_state = {0, 0, 0,
                                     0, 0, 0,
                                     0, 0, 0};
+    //print game board
+    std::cout << " tl | tm | tr" << std::endl
+              << "=============" << std::endl
+              << " ml | mm | mr" << std::endl
+              << "=============" << std::endl
+              << " bl | bm | br" << std::endl;
     
-    int d;
+    int move;
     while(gaming) {
-        d = get_move();
-        std::cout << d << '\n';
+        move = get_move();
+        if(validMove(game_state, move, ply1)) {
+            gaming = check_gaming(game_state,ply1);
+            print_board(game_state);
+            ply1 = !ply1;
+        }else {
+            std::cout << "space is occupied" << std::endl;
+        }
+
+        //std::cout << move << '\n';
+        
     }
 
-    std::cout << '\n';
+    std::cout << "game over, ";
+    if(ply1) {
+        std::cout << "player 1(X) wins!" << std::endl;
+    }else{
+        std::cout << "player 2(O) wins!" << std::endl;
+    }
     print_board(game_state);
 }
 
@@ -84,6 +112,19 @@ int get_move() {
     return index;
 }
 
+bool validMove(std::array<int, 9>& ar, int move, bool turn) {
+    if(ar[move] == 0) {
+        if(turn) {
+            ar[move] = 1;
+        }else{
+            ar[move] = 2;
+        }
+        return true;
+    }else{
+        
+        return false;
+    }
+}
 
 void print_board(std::array<int,9> ar) {
     char pl;
@@ -103,10 +144,41 @@ void print_board(std::array<int,9> ar) {
         }
     }
 }
+
+//true = game over, false = game ongoing
+bool check_gaming(std::array<int, 9> ar, bool turn) {
+    int bit = turn ? 1 : 2;
+
+    // check horizontal
+    for(int i = 0; i <= 6; i+=3) {
+        if(ar[i] == bit && ar[i+1] == bit && ar[i+2] == bit)
+            return false;
+    }
+
+    //check vertical
+    for(int i = 0; i <= 2; i++) {
+        if(ar[i] == bit && ar[i+3] == bit && ar[i+6] == bit)
+            return false;
+    }
+
+    // check diagonal
+    if(ar[0] == bit && ar[4] == bit && ar[8] == bit)
+        return false;
+
+    if(ar[2] == bit && ar[4] == bit && ar[6] == bit)
+        return false;
+
+    return true;
+}
+
 /*
 2 players
 
-
+ 0 | 1 | 2
+===========
+ 3 | 4 | 5
+===========
+ 6 | 7 | 8
 
  0 | 0 | 0
 ===========
