b56b3b22c89670eb04c88f4f308479c1e7e11d13
reticent-lily <bryantchen1807@gmail.com>
2025-04-23 19:39:20 -0400

    Update line_format.cpp
    improved?

--- line_format.cpp
+++ line_format.cpp
@@ -1,51 +1,72 @@
+// a program to format plain text files w/ whole text blocks
 #include <iostream>
-#include <ostream>
-#include <fstream>
 #include <string>
+#include <fstream>
 
+int check_ellipses(std::string str, int period);
 int main() {
+    //[[maybe_unused]] const double gravity {9.6};
+
     std::ifstream infile;
-    std::ofstream outfile("piss.txt");
+    std::ofstream outfile("form_text.txt");
+    std::string form_str {}; // holds all of infile
+    std::string temp{}; // holds infile text to be pushed
 
-    std::string piss;
-    int period = 100000, question = 100000, exclamation = 100000;
+    infile.open("bee_movie_script.txt");
+    std::getline(infile, form_str);
 
-    infile.open("rep/bee_movie_script.txt");
-    //infile.open("blargh.txt");
+    while(!form_str.empty()) {
+        int period = form_str.find('.');
+        int question = form_str.find('?');
+        int exclamation = form_str.find('!');
 
-    std::getline(infile, piss);
-    std::cout << piss.length() << std::endl;
-    
-    while(!piss.empty()) {
-        period = piss.find('.');
-        question = piss.find('?');
-        exclamation = piss.find('!');
-        
-        
-        if(period < question && period < exclamation) {
-            std::cout << piss.substr(0,period+2) << std::endl;
-            outfile << piss.substr(0,period+2);
-            outfile << '\n';
-            piss.erase(0,period+2);
-        }else if(question < exclamation) {
-            std::cout << piss.substr(0,question+2) << std::endl;
-            outfile << piss.substr(0,question+2);
-            outfile << '\n';
-            piss.erase(0,question+2);
-        }else{
-            std::cout << piss.substr(0,exclamation+2) << std::endl;
-            outfile << piss.substr(0,exclamation+2);
-            outfile << '\n';
-            piss.erase(0,exclamation+2);
+        if(question != std::string::npos || exclamation != std::string::npos) {
+            if(question < period && question < exclamation) {
+                temp = form_str.substr(0, question + 2);
+                form_str.erase(0, question + 2);
+                outfile << temp << '\n';
+                continue;
+            }
+            if(exclamation < period && exclamation < question) {
+                temp = form_str.substr(0, exclamation + 2);
+                form_str.erase(0, exclamation + 2);
+                outfile << temp << '\n';
+                continue;
+            }
         }
+
+        temp = form_str.substr(0, period + check_ellipses(form_str, period) + 1);
+        form_str.erase(0, period + check_ellipses(form_str, period) + 1);
+        outfile << temp << '\n';
+        std::cout << temp << std::endl;
     }
-        
-    std::cout << std::string::npos;
-    
+
+    //std::cout << form_str.length() << std::endl;
+    //std::cout << check_ellipses(form_str, 0) << std::endl;
+
     infile.close();
     outfile.close();
 }
+
+// a function to check for ellipses
+int check_ellipses(std::string str, int period) {
+    int end {1};
+    while(str.length()  >= (period + end) && str[period + end] == '.'){
+        if(str[period + end] == '.')
+            end++;
+    }
+    
+    return end;
+    //d
+}
+
+
+
 /*
-read the text
-separate after each '.'
+find
+substr
+erase, erase(pos, n)
+empty ?
+swap
+insert(pos, str)
 */
