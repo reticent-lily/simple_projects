5ae27a487a422f0a8767002f9a354764dc6cd1a0
reticent-lily <bryantchen1807@gmail.com>
2025-09-07 06:27:32 -0500

    Update main.cpp

--- reConway/main.cpp
+++ reConway/main.cpp
@@ -4,17 +4,31 @@ int main() {
     const int screenWidth = 1000;
     const int screenHeight = 1000;
 
+    const int cellDimension = 15; // dimension of the living cells
+
+    const int xOffset = 500; // horizontal offset of the living cells
+    const int yOffset = 500; // vertical offset of the living cells
+    
+    // living cells
+    std::vector<cell> livingCells = {{0, 0}, {1,0}, {-1,1}, {0,1}}; 
+    
+    
     InitWindow(screenWidth, screenHeight, "Conway's Game of Life");
     SetTargetFPS(60);
+    std::cout << countNeighborCells(&livingCells, livingCells[0]) << std::endl;
 
+    
     while(!WindowShouldClose()) {
+        
         BeginDrawing();
             ClearBackground(LIGHTGRAY);
-            DrawRectangle(screenWidth/2, screenHeight/2, 15, 15, MAROON);
+            drawLivingCells(&livingCells, xOffset, yOffset, cellDimension);
+
         EndDrawing();
     }
     
     CloseWindow();
+    
 }
 
 //g++ main.cpp conway_func.cpp -o conway -IC:\libraries\raylib\raylib\src -LC:\libraries\raylib\raylib\src -lraylib -lopengl32 -lwinmm -lgdi32
