0c1d80ff59da424359880a880c7fcb5cb41bcb13
reticent-lily <bryantchen1807@gmail.com>
2025-09-07 06:23:50 -0500

    Update conway_func.cpp

--- reConway/conway_func.cpp
+++ reConway/conway_func.cpp
@@ -1,1 +1,20 @@
-#include "conway_func.h"
+#ifndef CONWAY_FUNCTIONS
+#define CONWAY_FUNCTIONS
+
+#include "iostream"
+#include "raylib.h"
+#include "vector"
+
+struct cell {
+    int m_x;
+    int m_y;
+};
+
+cell operator+(cell& a, cell& b);
+cell operator-(cell& a, cell& b);
+bool operator==(cell& a, cell& b);
+
+void drawLivingCells(std::vector<cell>* ptrToLivingCells, int xOffset, int yOffset, int cellDimension) ; // iterates through vector and draws cells accordingly
+int countNeighborCells(std::vector<cell>* ptrToLivingCells, cell curCell); // counts and returns the number of living cells surrounding 'curCell' in the vector
+
+#endif
