6019f9c8ee6e55ed812d4f1ddb276e0dec01cf04
reticent-lily <bryantchen1807@gmail.com>
2025-09-10 00:59:43 -0500

    Update conway_func.h

--- reConway/conway_func.h
+++ reConway/conway_func.h
@@ -16,6 +16,12 @@ cell operator-(cell& a, cell& b);
 bool operator==(cell& a, cell& b);
 
 void drawLivingCells(std::vector<cell>* ptrToLivingCells, int xOffset, int yOffset, int cellDimension) ; // iterates through vector and draws cells accordingly
-void countNeighborCells(std::vector<cell>* ptrToLivingCells, cell curCell); // counts and sets the number of living cells surrounding 'curCell' in the vector
+void countNeighborCells(std::vector<cell>* ptrToLivingCells, cell& curCell); // counts and sets the number of living cells surrounding 'curCell' in the vector
+bool isCellInVector(std::vector<cell>* searchVector, cell queryCell);
+
+bool isCellLiving(std::vector<cell>* ptrToLivingCells, cell queryCell);
+void iterateThroughLiving(std::vector<cell>* ptrToLivingCells, std::vector<cell>* tempStorageVector);
+void iterateThroughDead(std::vector<cell>* ptrToLivingCells, std::vector<cell>* tempStorageVector);
+void doConway(std::vector<cell>* ptrToLivingCells);
 
 #endif
